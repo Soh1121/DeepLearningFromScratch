57a2185c66950c14fd560eacdee92f65b57e01ff
Soh1121 <satou.shg@gmail.com>
2020-08-02 15:28:52 +0900

    [add]3層ニューラルネットワークの実装

--- ch03/sigmoid.py
+++ ch03/sigmoid.py
@@ -6,8 +6,9 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-X = np.arange(-5.0, 5.0, 0.1)
-Y = sigmoid(X)
-plt.plot(X, Y)
-plt.ylim(-0.1, 1.1)
-plt.show()
+if __name__ == "__main__":
+    X = np.arange(-5.0, 5.0, 0.1)
+    Y = sigmoid(X)
+    plt.plot(X, Y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
