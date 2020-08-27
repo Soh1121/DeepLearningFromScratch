a6f7d4f4b54424c50230e2f0ab102003327e4816
Soh1121 <satou.shg@gmail.com>
2020-08-27 11:11:05 +0900

    [update]Convolutionレイヤの実装

--- common/layers.py
+++ common/layers.py
@@ -183,3 +183,25 @@ class Dropout:
 
     def backward(self, dout):
         return dout * self.mask
+
+
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = 1 + int((H + 2 * self.pad - FH) / self.stride)
+        out_w = 1 + int((W + 2 * self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T
+        out = np.dot(col, col_W) + self.b
+
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        return out
